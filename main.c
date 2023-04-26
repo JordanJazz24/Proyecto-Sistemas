309fca93bca4c05ee3d703afc77c74ff8dce5e0b
JordanJazz24 <jazzman24@protonmail.com>
2023-04-25 18:19:33 -0600

    Casi terminado

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
 #include <pthread.h>
 
-//Definimos la estructura de datos que utilizaremos para enviar los argumentos a los hilos
-typedef struct {
+#define MAX_THREADS 8
+
+// Estructura para almacenar los parámetros que se le pasan a cada hilo
+struct thread_data {
     long *vector;
     long m;
     long n;
-} quicksort_args;
+};
 
-//Función para intercambiar 2 elementos
+// Función para intercambiar dos elementos
 void swap(long *x, long *y) {
     long temp;
     temp = *x;
@@ -20,26 +21,19 @@ void swap(long *x, long *y) {
     *y = temp;
 }
 
-//Función para seleccionar como el pivote el elemento central del vector
+// Función para seleccionar como el pivote el elemento central del vector
 long choose_pivot(long i, long j) {
     return ((i + j) / 2);
 }
 
-//Función que muestra el vector en pantalla
-void printvector(long vector[], long n) {
-    long i;
-    for (i = 0; i < n; i++)
-        printf("%ld %ld\n", i, vector[i]);
-}
-
-//Función para obtener el tiempo de duración de ordenamiento del vector.
+// Función para obtener el tiempo de duración de ordenamiento del vector.
 double timeval_diff(struct timeval *a, struct timeval *b) {
     return
             (double) (a->tv_sec + (double) a->tv_usec / 1000000) -
             (double) (b->tv_sec + (double) b->tv_usec / 1000000);
 }
 
-//Función que realiza el ordenamiento mediante el Quicksort
+// Función para realizar el ordenamiento mediante el Quicksort
 void quicksort(long vector[], long m, long n) {
     long key, i, j, k;
     if (m < n) {
@@ -53,75 +47,127 @@ void quicksort(long vector[], long m, long n) {
             while ((j >= m) && (vector[j] > key))
                 j--;
             if (i < j)
+                //Intercambia (swap) dos elementos
                 swap(&vector[i], &vector[j]);
         }
+        //Intercambia (swap) dos elementos
         swap(&vector[m], &vector[j]);
+        // Efectúa los llamados recursivos a ambos vectores
         quicksort(vector, m, j - 1);
         quicksort(vector, j + 1, n);
     }
 }
 
-//Función que se ejecuta en el hilo 1
-void *quicksort_thread1(void *arguments) {
-    quicksort_args *args = (quicksort_args *) arguments;
-    quicksort(args->vector, args->m, args->n / 2);
+// Función para realizar el ordenamiento en paralelo mediante hilos
+void *quicksort_threaded(void *args) {
+    // Obtener los argumentos pasados al hilo
+    struct thread_data *data = (struct thread_data *) args;
+    long *vector = data->vector;
+    long m = data->m;
+    long n = data->n;
+
+    // Ordenar la sublista utilizando el Quicksort
+    quicksort(vector, m, n);
+
+    // Terminar el hilo
     pthread_exit(NULL);
 }
 
-//Función que se ejecuta en el hilo 2
-void *quicksort_thread2(void *arguments) {
-    quicksort_args *args = (quicksort_args *) arguments;
-    quicksort(args->vector, args->n / 2 + 1, args->n);
-    pthread_exit(NULL);
-}
 
-//===============================================
-//Función Principal
 
-int main() {
-    //Tamaño del vector
+
+
+
+// Función principal
+int main(void) {
+// Tamaño del vector
     long const MAX_ELEMENTS = 10;
-    //Reserva un espacio de memoria X cantidad de elementos de tipo long
+
+// Reserva un espacio de memoria X cantidad de elementos de tipo long
     long *vector = malloc(MAX_ELEMENTS * sizeof(long));
 
-    struct timeval t_ini, t_fin;
-    double secs;
-    long i = 0;
-
-    //Genera los números aleatorios para llenar el vector
+// Genera los números aleatorios para llenar el vector
     long iseed = (long) time(NULL);
     srand(iseed);
-    for (i = 0; i < MAX_ELEMENTS; i++) {
+    for (long i = 0; i < MAX_ELEMENTS; i++) {
         vector[i] = rand() % 999999999999; //Números grandes (12 dígitos)
-        //vector[i] = rand() % 99; //Números (2 dígitos)
     }
 
-    //Muestra el vector antes de ordenarse
+// Muestra el vector antes de ordenarse
     printf("\nMuestra el vector antes de ordenarse:\n");
-    printvector(vector, MAX_ELEMENTS);
+    for (long i = 0; i < MAX_ELEMENTS; i++) {
+        printf("%ld %ld\n", i, vector[i]);
+    }
 
-    //Creamos los argumentos para los hilos
-    quicksort_args args1 = {vector, 0, MAX_ELEMENTS - 1};
-    quicksort_args args2 = {vector, 0, MAX_ELEMENTS - 1};
+// Declarar variables para medir el tiempo de ejecución
+    struct timeval t_ini, t_fin;
+    double secs;
 
-    //Creamos los hilos y los ejecutamos
-    pthread_t thread1, thread2;
+// Variables para manejar los hilos
+    pthread_t threads[MAX_THREADS];
+    struct thread_data thread_data_array[MAX_THREADS];
+    long chunk_size = MAX_ELEMENTS / MAX_THREADS;
+
+// Iniciar el contador de tiempo
     gettimeofday(&t_ini, NULL);
-    pthread_create(&thread1, NULL, quicksort_thread1, (void *) &args1);
-    pthread_create(&thread2, NULL, quicksort_thread2, (void *) &args2);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+
+// Crear los hilos
+    for (long i = 0; i < MAX_THREADS; i++) {
+// Configurar los parámetros para el hilo actual
+        long m = i * chunk_size;
+        long n = (i == MAX_THREADS - 1) ? MAX_ELEMENTS - 1 : m + chunk_size - 1;
+        thread_data_array[i].vector = vector;
+        thread_data_array[i].m = m;
+        thread_data_array[i].n = n;
+        // Crear el hilo
+        int rc = pthread_create(&threads[i], NULL, quicksort_threaded, (void *) &thread_data_array[i]);
+        if (rc) {
+            printf("Error: no se pudo crear el hilo %ld.\n", i);
+            exit(-1);
+        }
+    }
+// Esperar a que terminen todos los hilos
+    for (long i = 0; i < MAX_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+// Combinar las sub-listas ordenadas por los hilos en un solo vector
+    long sorted_vector[MAX_ELEMENTS];
+    long pos[MAX_THREADS];
+    for (long i = 0; i < MAX_THREADS; i++) {
+        pos[i] = thread_data_array[i].m;
+    }
+    for (long i = 0; i < MAX_ELEMENTS; i++) {
+        long min_val = vector[MAX_ELEMENTS - 1];
+        long min_idx = -1;
+        for (long j = 0; j < MAX_THREADS; j++) {
+            if (pos[j] <= thread_data_array[j].n && vector[pos[j]] < min_val) {
+                min_val = vector[pos[j]];
+                min_idx = j;
+            }
+        }
+        sorted_vector[i] = min_val;
+        pos[min_idx]++;
+    }
+
+// Copiar el vector ordenado en el vector original
+    for (long i = 0; i < MAX_ELEMENTS; i++) {
+        vector[i] = sorted_vector[i];
+    }
+
+// Detener el contador de tiempo y calcular el tiempo de ejecución
     gettimeofday(&t_fin, NULL);
+    secs = timeval_diff(&t_fin, &t_ini);
 
-    //Mostramos el vector ordenado
-    printf("\nMuestra el vector despues de ordenarse:\n");
-    printvector(vector, MAX_ELEMENTS - 1);
+// Muestra el vector después de ordenarse
+    printf("\nMuestra el vector después de ordenarse:\n");
+    for (long i = 0; i < MAX_ELEMENTS; i++) {
+        printf("%ld %ld\n", i, vector[i]);
+    }
 
-    //Calculamos el tiempo de ejecución
-    secs = timeval_diff(&t_ini, &t_fin);
-    printf("Tiempo de ejecucion: %f segundos\n", (secs * 1000.0) * -1);
-
-    //Liberamos la memoria reservada para el vector
+// Muestra el tiempo de duración del ordenamiento
+    printf("%.4g milliseconds\n", secs * 1000.0);
+// Libera el espacio de memoria reservado para el vector
     free(vector);
 
     return 0;
